37647c9f084eca301098bc7776985d6064bc938f
Igor <i.gorbatenco@gmail.com>
2025-08-10 02:07:21 -0400

    1st

---
+++ catalog/views.py
@@ -0,0 +1,16 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'catalog/home.html')
+
+
+def contacts(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+
+        return HttpResponse(f"Спасибо, {name}! Ваше сообщение получено!")
+
+    return render(request, 'catalog/contacts.html')
